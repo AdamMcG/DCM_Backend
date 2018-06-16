be0a25c19267b9962189cafbcddcea68a293c0f0
AdamMcGivern <x00098814@postgrad.ittdublin.ie>
2018-06-16 21:22:56 +0100

    Initial commit for Spring project

---
+++ src/main/java/com/DCM/DCM_backend/DcmBackendApplication.java
@@ -0,0 +1,12 @@
+package com.DCM.DCM_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcmBackendApplication.class, args);
+	}
+}
